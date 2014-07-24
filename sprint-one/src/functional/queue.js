20332f8b1455e8bdeaf37a9d24d176af1e924af5
Hack Reactor Students <klp2012@gmail.com>
2014-07-24 09:46:17 -0700

    functional instantiation - queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,27 @@ var makeQueue = function(){
   var storage = {};
 
   // Implement the methods below
+  var size = 0;
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+      result = storage[0];
+      delete storage[0];
+      for(var i = 0; i < size-1; i++){
+        storage[i] = storage[i+1];
+      }
+      if(size > 0){
+        size--;
+      }
+      return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
