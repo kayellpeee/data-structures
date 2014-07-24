3d5ad62aa7a813a3df443866dc5d202bc417f07f
Hack Reactor Students <klp2012@gmail.com>
2014-07-24 09:27:30 -0700

    functional shared instantiation - stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.push = stackMethods.push;
+  stack.pop = stackMethods.pop;
+  stack.size = stackMethods.size;
+  stack.stackSize = 0;
+  stack.storage = {};
+  return stack;
 };
 
 var stackMethods = {};
 
+  stackMethods.push = function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  }
 
+  stackMethods.pop = function() {
+    if(this.stackSize > 0) {
+      this.stackSize--;
+    }
+    var result = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return result;
+  }
+
+  stackMethods.size = function() {
+    return this.stackSize;
+  }
