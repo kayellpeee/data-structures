fcd3cdc04ced5a01cb5b71886d1818e40b282a21
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-27 11:44:07 -0700

    solves functional stack instantiation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    size++;
+    storage[size] = value;
   };
 
   someInstance.pop = function(){
+    if( storage[size] ){
+      var temp = storage[size];
+      delete storage[size];
+      size--;
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
