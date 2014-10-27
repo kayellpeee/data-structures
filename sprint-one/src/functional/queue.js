b767ed43295cf76d2258d9382f0ce85f61d80f94
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-27 13:26:28 -0700

    solves functional queue instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,26 @@ var makeQueue = function(){
   var storage = {};
 
   // Implement the methods below
-
+  var size = 0;
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    if( size ){
+      var temp = storage["0"];
+      for( var i = 0; i < size; i++ ){
+        storage[i] = storage[i + 1];
+      }
+      delete storage[size];
+      size--;
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
