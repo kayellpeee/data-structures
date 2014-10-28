d82b4dab1b3ff3fd8d011e14af38e6e5bcbf3462
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-28 11:19:22 -0700

    solves prototypal stack instantiation

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.capacity = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.capacity] = value;
+    this.capacity++;
+  },
+  pop: function(){
+    if( this.capacity ){
+      this.capacity--;
+      var temp = this.storage[this.capacity];
+      delete this.storage[this.capacity];
+      return temp;
+    }
+  },
+  size: function(){
+    return this.capacity;
+  }
+};
 
 
