7aa78b149a1b78f61e7099b3f0259f4f86693152
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-28 14:21:03 -0700

    implements a set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = "";
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if( this._storage.indexOf(item) === -1 ){
+    this._storage += item;
+  }
 };
 
 setPrototype.contains = function(item){
+  if( this._storage.indexOf(item) !== -1 ){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  this._storage = this._storage.split(item).join("");
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add - linear
+ * contains - linear
+ * remove - linear
  */
