53e3ce942fda8cf71db1985e728ab9e1c7dd8d59
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-28 10:57:34 -0700

    solves functional-shared stack instantiation

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    push: stackMethods.push,
+    pop: stackMethods.pop,
+    size: stackMethods.size,
+    storage: {},
+    capacity: 0
+  }
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.capacity] = value;
+    this.capacity++;
+  },
+  pop: function(){
+    if( this.capacity ){
+      this.capacity--;
+      var temp = this.storage[this.capacity];
+      delete this.storage[this.capacity];
+      return temp;
+    }
+  },
+  size: function(){
+    return this.capacity;
+  }
+};
 
 
