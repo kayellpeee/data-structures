3e5d68f35318aadc7bb1736ba7833d42051af40a
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-28 15:10:03 -0700

    implements a hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,19 +5,42 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = this._storage[i] || [];
+  this._storage[i].push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  if( !this._storage[i] ){
+    return null;
+  }
+  var bucket = this._storage[i];
+  for( var j = 0; j < bucket.length; j++ ){
+    var tuple = bucket[j];
+    if( tuple[0] === k ){
+      return tuple[1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  if( this._storage[i] ){
+    var bucket = this._storage[i];
+    for( var j = 0; j < bucket.length; j++ ){
+      var tuple = bucket[j];
+      if( tuple[0] === k ){
+        delete this._storage[i];
+      }
+    }
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert - constant
+ * retrieve - constant
+ * remove - constant ?
  */
