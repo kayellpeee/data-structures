b4e48496717468c9184bbd21e9d0b8958ba866f1
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-28 15:46:15 -0700

    implements tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,8 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   return newTree;
 };
 
@@ -11,14 +13,29 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children = this.children || [];
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
-
+  var found = false;
+  var recurse = function(node){
+    if( node.value === target ){
+      return found = true;
+    }
+    if( node.children ){
+      for( var i = 0; i < node.children.length; i++ ){
+        recurse(node.children[i]);
+      }
+    }
+  }
+  recurse(this);
+  return found;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild - constant
+ * contains - linear
  */
