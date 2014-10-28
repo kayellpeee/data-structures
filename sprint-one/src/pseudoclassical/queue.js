fcf4a67be82096062ce8d24786f236c0bb8f576f
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-28 11:24:44 -0700

    solves pseudoclassical queue instantiation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {}
+  this.capacity = 0;
 };
 
+Queue.prototype.enqueue = function(value){
+  this.storage[this.capacity] = value;
+  this.capacity++;
+};
+Queue.prototype.dequeue = function(){
+  if( this.capacity ){
+    var temp = this.storage["0"];
+    for( var i = 0; i < this.capacity; i++ ){
+      this.storage[i] = this.storage[i + 1];
+    }
+    delete this.storage[this.capacity];
+    this.capacity--;
+    return temp;
+  }
+};
+Queue.prototype.size = function(){
+  return this.capacity;
+};
 
