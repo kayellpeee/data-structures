e9a542443320d5cd1e1bd88ba47b5018761e4d17
Keenan Lidral-Porter <klp2012@gmail.com>
2014-11-12 11:23:18 -0800

    adds spec for parent links on trees

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -5,9 +5,10 @@ describe('tree', function() {
     tree = makeTree();
   });
 
-  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+  it('should have methods named "addChild", "removeFromParent" and "contains", and a property named "value"', function() {
     expect(tree.addChild).to.be.a("function");
     expect(tree.contains).to.be.a("function");
+    expect(tree.removeFromParent).to.be.a("function");
     expect(tree.hasOwnProperty("value")).to.equal(true);
   });
 
@@ -39,6 +40,27 @@ describe('tree', function() {
     tree.children[1].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+  });
+
+  it('should correctly detect parent nodes', function(){
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[1].addChild(7);
+    var parent = tree.children[1];
+    expect(parent.children[0].parent.value).to.equal(6);
+  });
+
+  it('should remove a tree from it\'s parent', function(){
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    expect(tree.children[0].children[0].value).to.equal(7);
+    var grandchild = tree.children[0].children[0];
+    grandchild.removeFromParent();
+    expect(grandchild.parent).to.be.null;
+    expect(tree.children[0].children).to.be.null;
+    tree.children[1].removeFromParent();
+    expect(tree.children.length).to.equal(1);
   });
 
 });
